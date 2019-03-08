e4c3178f02e234292e594af6a2afb99aabe95a7e
rons1 <ron.shmuter@gmail.com>
2019-03-08 16:19:57 +0200

    Ron: Validate update user form

--- BurgeRate/MyProfileViewController.swift
+++ BurgeRate/MyProfileViewController.swift
@@ -16,24 +16,46 @@ class MyProfileViewController: UIViewController {
     
     @IBOutlet weak var UsernameUpdateText: UITextField!
     @IBOutlet weak var GenderUpdagteSeg: UISegmentedControl!
+    @IBOutlet weak var UsernameUpdateVal: UILabel!
+    
+    func validateUsername(username: String) -> Bool {
+        if username == "" {
+            UsernameUpdateVal.text = "Username can't be empty"
+            return false
+        }
+        if username.count < 5 {
+            UsernameUpdateVal.text = "Username can't be less then 5 characters"
+            return false
+        }
+        
+        if username.count > 20 {
+            UsernameUpdateVal.text = "Username can't be more then 20 characters"
+            return false
+        }
+        
+        UsernameUpdateVal.text = ""
+        return true
+    }
     
     @IBAction func UpdateUser(_ sender: Any) {
-        Model.instance.updateUser(username: UsernameUpdateText.text!, gender: GenderUpdagteSeg.selectedSegmentIndex) { (success) in
-            if success {
-                
-                let alertController = UIAlertController(title: "User updated", message:
-                    "User updated successfully", preferredStyle: UIAlertController.Style.alert)
-                alertController.addAction(UIAlertAction(title: "Back", style: UIAlertAction.Style.default,handler: nil))
-                self.present(alertController, animated: true, completion: nil)
-                sleep(1)
- 
-                Model.instance.updateCurrentUser()
-                
-            } else {
-                let alertController = UIAlertController(title: "User update", message:
-                    "User update failed", preferredStyle: UIAlertController.Style.alert)
-                alertController.addAction(UIAlertAction(title: "Back", style: UIAlertAction.Style.default,handler: nil))
-                self.present(alertController, animated: true, completion: nil)
+        if validateUsername(username: UsernameUpdateText.text!) {
+            Model.instance.updateUser(username: UsernameUpdateText.text!, gender: GenderUpdagteSeg.selectedSegmentIndex) { (success) in
+                if success {
+                    
+                    let alertController = UIAlertController(title: "User Update", message:
+                        "User updated successfully", preferredStyle: UIAlertController.Style.alert)
+                    alertController.addAction(UIAlertAction(title: "Back", style: UIAlertAction.Style.default,handler: nil))
+                    self.present(alertController, animated: true, completion: nil)
+                    sleep(1)
+     
+                    Model.instance.updateCurrentUser()
+                    
+                } else {
+                    let alertController = UIAlertController(title: "User Update", message:
+                        "User update failed", preferredStyle: UIAlertController.Style.alert)
+                    alertController.addAction(UIAlertAction(title: "Back", style: UIAlertAction.Style.default,handler: nil))
+                    self.present(alertController, animated: true, completion: nil)
+                }
             }
         }
     }
